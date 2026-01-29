47ae01eef5590210ed4d55d68bd93deabab64d3e
Sulthan <thanzero7@gmail.com>
2026-01-29 09:51:06 +0700

    button function js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const buttons = document.querySelector(".buttons");
 let angka1 = "";
 let angka2 = "";
 let operator = "";
+
+buttons.addEventListener("click", function (e) {
+    const button = e.targer;
+})
